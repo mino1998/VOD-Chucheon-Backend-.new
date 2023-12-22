452c61d0cfcbdd99260d3f941278622ed953f35f
sbkim <kjb7506@ajou.ac.kr>
2023-12-22 18:50:50 +0900

    s3 수정

--- mainpage/models.py
+++ mainpage/models.py
@@ -1,5 +1,3 @@
 from django.db import models
 
-class AWSAuth(models.Model):
-    access_key_id = models.CharField(max_length=80)
-    access_secret_key_id = models.CharField(max_length=80)
+# Create your models here.
